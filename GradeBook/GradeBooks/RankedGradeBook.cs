f8514796d9eb4929aa5f26a7f8fc17ad069a1cc7
Lishjones <49446723+Lishjones@users.noreply.github.com>
2019-04-15 13:26:19 +0100

    Completed override changes module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,12 +1,36 @@
 ﻿namespace GradeBook.GradeBooks
 {
     using GradeBook.Enums;
+    using System;
+    using System.Linq;
 
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grades need at least 5 student's grades");
+            }
+
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
